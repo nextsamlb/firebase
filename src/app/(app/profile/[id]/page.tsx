53d1800f302ad42c90627548a73d84bf4a1085d6
Sam Younes <tasks.samehyounes@gmail.com>
2025-09-25 03:32:38 +0000

    move all foders and files from (app) to app directory

--- src/app/(app/profile/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-
-import PlayerProfileClient from './client';
-
-export default function PlayerProfilePage({ params: { id } }: { params: { id: string } }) {
-  return <PlayerProfileClient playerId={id} />;
-}
